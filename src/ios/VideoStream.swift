3512437f2c9ad7019260157792eb225633131347
Ramprasad <ramprasad@krititech.in>
2020-09-18 18:41:51 +0530

    Bug Fixes

--- src/ios/VideoStream.swift
+++ src/ios/VideoStream.swift
@@ -3,6 +3,15 @@ import HaishinKit
 import UIKit
 
 @objc(VideoStream) class VideoStream : CDVPlugin {
+    
+    var rtmpConnection: RTMPConnection? = nil
+    var rtmpStream: RTMPStream? = nil
+    var hkView : HKView? = nil
+    var closeButton: UIButton? = nil
+    var streamName: String = ""
+    var command:CDVInvokedUrlCommand? = nil
+  
+  @objc(echo:)
   func echo(_ command: CDVInvokedUrlCommand) {
     var pluginResult = CDVPluginResult(
       status: CDVCommandStatus_ERROR
@@ -10,7 +19,7 @@ import UIKit
 
     let msg = command.arguments[0] as? String ?? ""
 
-    if msg.characters.count > 0 {
+    if msg.count > 0 {
       /* UIAlertController is iOS 8 or newer only. */
       let toastController: UIAlertController = 
         UIAlertController(
@@ -47,7 +56,8 @@ import UIKit
   }
 
   @objc(streamRTMP:)
-    func streamRTMP(_ command: CDVInvokedUrlCommand) {
+func streamRTMP(_ command: CDVInvokedUrlCommand) {
+    self.command = command
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
             )
@@ -55,68 +65,49 @@ import UIKit
         let uri = command.arguments[0] as? String ?? ""
         let streamName = command.arguments[1] as? String ?? ""
    
-        let session = AVAudioSession.sharedInstance()
-        do {
-            
-            if #available(iOS 10.0, *) {
-                try session.setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker, .allowBluetooth])
-            } else {
-                session.perform(NSSelectorFromString("setCategory:withOptions:error:"), with: AVAudioSession.Category.playAndRecord, with: [
-                    AVAudioSession.CategoryOptions.allowBluetooth,
-                    AVAudioSession.CategoryOptions.defaultToSpeaker]
-                )
-                try session.setMode(.default)
-            }
-            try session.setActive(true)
-        } catch {
-            print(error)
-        }
-    
+        self.streamName=streamName
         
-        rtmpConnection = RTMPConnection()
-        rtmpStream = RTMPStream(connection: rtmpConnection!)
-        rtmpStream?.attachAudio(AVCaptureDevice.default(for: AVMediaType.audio)) { error in
+        self.rtmpConnection = RTMPConnection()
+        self.rtmpStream = RTMPStream(connection: rtmpConnection!)
+        self.rtmpStream?.attachAudio(AVCaptureDevice.default(for: AVMediaType.audio)) { error in
             print(error)
         }
-        rtmpStream?.attachCamera(DeviceUtil.device(withPosition: .back)) { error in
+       self.rtmpStream?.attachCamera(DeviceUtil.device(withPosition: .back)) { error in
             print(error)
         }
         let view = viewController.view
-        hkView = HKView(frame: view!.bounds)
-        hkView?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        hkView?.attachStream(rtmpStream)
+        self.hkView = HKView(frame: view!.bounds)
+        self.hkView?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.hkView?.attachStream(rtmpStream)
 
     
-        view?.addSubview(hkView!)
+        view?.addSubview(self.hkView!)
         
-       closeButton = UIButton(type: .system)
+       self.closeButton = UIButton(type: .system)
        
        // Position Button
-       closeButton?.frame = CGRect(x: 20, y: 20, width: 100, height: 50)
-       closeButton.center = view.center
+        self.closeButton?.frame = CGRect(x: 20, y: 20, width: 100, height: 50)
+   
        // Set text on button
-       closeButton?.setTitle("Close", for: .normal)
-       closeButton?.setTitle("Close", for: .highlighted)
+        self.closeButton?.setTitle("Close", for: .normal)
+        self.closeButton?.setTitle("Close", for: .highlighted)
        
        // Set button action
       
        
-        view?.addSubview(closeButton!)
+        view?.addSubview(self.closeButton!)
   
         
         UIApplication.shared.isIdleTimerDisabled = true
         
-        rtmpConnection?.connect(uri)
+        self.rtmpConnection?.connect(uri)
         
-        closeButton?.addTarget(self, action: #selector(closeAction(_:)), for: .touchUpInside)
+        self.closeButton?.addTarget(self, action: #selector(closeAction(_:)), for: .touchUpInside)
         
+   
+        self.rtmpStream!.publish(self.streamName)
         
-        rtmpStream?.publish(streamName)
-        
-        
-        
-        
-        
+       
         
         pluginResult = CDVPluginResult(
             status: CDVCommandStatus_OK,
@@ -131,6 +122,14 @@ import UIKit
     
     @objc private func closeAction(_ sender: UIButton?) {
         
+        
+        self.stopStream()
+        
+        
+    }
+    
+    @objc private func stopStream(){
+        
         rtmpConnection?.close()
         rtmpStream?.close()
         rtmpStream?.dispose()
@@ -140,5 +139,68 @@ import UIKit
         hkView?.removeFromSuperview()
         
         UIApplication.shared.isIdleTimerDisabled = false
+        
+    }
+    
+    @objc private func alerMessage(msg: String){
+        
+        if msg.count > 0 {
+          /* UIAlertController is iOS 8 or newer only. */
+          let toastController: UIAlertController =
+            UIAlertController(
+              title: "",
+              message: msg,
+              preferredStyle: .alert
+            )
+
+          self.viewController?.present(
+            toastController,
+            animated: true,
+            completion: nil
+          )
+
+            let mainQueue = DispatchQueue.main
+            let deadline = DispatchTime.now() + .seconds(5)
+            mainQueue.asyncAfter(deadline: deadline) {
+                toastController.dismiss(
+                    animated: true,
+                    completion: nil
+                )
+            }
+
+          
+        }
+        
+    }
+    
+    @objc func rtmpStatusEvent(_ notification: Notification) {
+    let e: Event = Event.from(notification)
+    if let data: ASObject = e.data as? ASObject, let code: String = data["code"] as? String{
+            // you can handle errors.
+            switch code {
+            case RTMPConnection.Code.connectSuccess.rawValue:
+                self.alerMessage(msg: "Connected")
+                break
+            case RTMPConnection.Code.connectRejected.rawValue:
+                self.alerMessage(msg: "Unbleto Connect To Server")
+                break
+            case RTMPConnection.Code.connectClosed.rawValue:
+                self.alerMessage(msg: "Disconnected")
+                break
+            case RTMPConnection.Code.connectFailed.rawValue:
+                self.alerMessage(msg: "Unbleto Connect To Server")
+                break
+            case RTMPConnection.Code.connectIdleTimeOut.rawValue:
+                self.alerMessage(msg: "Server Connection Time Out")
+                break
+            case RTMPStream.Code.publishStart.rawValue:
+                self.alerMessage(msg: "Streaming Started")
+                break
+            case RTMPStream.Code.unpublishSuccess.rawValue:
+                break
+            default:
+                break
+            }
+        }
     }
 }
